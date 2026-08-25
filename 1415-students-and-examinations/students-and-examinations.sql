05df7e3077a3dbc34059b8f4ae17f0a5b4b478b1
Trishanjit Goswami <trishanjitgoswami@gmail.com>
2026-08-25 17:57:46 +0530

    Time: 1160 ms (42.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,1 @@
+select s.student_id,s.student_name ,su.subject_name ,count(e.student_id) as attended_exams from Students s cross join Subjects su left join Examinations e on s.student_id= e.student_id  and su.subject_name =e.subject_name  group by s.student_id, SU.subject_name order by s.student_id, SU.subject_name ;
