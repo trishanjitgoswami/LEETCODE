2b3607d9fb4d9e74e28d01daf416269623e0ff34
Trishanjit Goswami <trishanjitgoswami@gmail.com>
2026-08-26 15:54:01 +0530

    Time: 903 ms (5.58%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -0,0 +1,1 @@
+select l.book_id,title ,author ,genre,publication_year, total_copies as current_borrowers from library_books l join borrowing_records b on l.book_id =b.book_id and  return_date is null group by l.book_id having(count(b.book_id))=total_copies order by  current_borrowers desc, title ;
